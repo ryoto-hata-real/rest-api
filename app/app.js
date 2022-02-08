98edd8030d11528db5f25ff2dcc15175eaaf9cd6
ryoto-hata-real <ryotoht1203@gmail.com>
2022-02-08 22:07:13 +0900

    create user form

--- app/app.js
+++ app/app.js
@@ -9,6 +9,11 @@ const path = require('path')
     // set static root directory
 app.use(express.static(path.join(__dirname, 'public')))
 
+// read request
+const bodyParser = require('body-parser')
+    // Setting:Parse Request-Body 
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 // Get all users
 app.get('/api/v1/users',(req, res) => {
@@ -33,18 +38,7 @@ app.get('/api/v1/users/:id',(req, res) => {
     db.close()
 })
 
-// Search users matching keyword
-// app.get('/api/v1/search',(req, res) => {
-//     const db = new sqlite3.Database(dbPath)
-//     const keyword = req.query.q
-//     console.log(keyword)
 
-//     db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
-//         res.json(rows)
-//     })
-
-//     db.close()
-// })
 app.get('/api/v1/search', (req, res) => {
     // Connect database
     const db = new sqlite3.Database(dbPath)
@@ -56,6 +50,66 @@ app.get('/api/v1/search', (req, res) => {
   
     db.close()
   })
+
+// Create a new User
+app.post('/api/v1/users', async (req, res) => {
+    const db = new sqlite3.Database(dbPath)
+
+    const name = req.body.name
+    const profile = req.body.profile ? req.body.profile : ""
+    const date_of_birth = req.body.date_of_birth ? req.body.date_of_birth : ""
+
+    
+
+    await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${date_of_birth}")`, db, res, "新規ユーザを作成しました。")
+    .then((code) => { console.log("normal:" + code); })
+    .catch((code) => { console.error("error:" + code); })
+    db.close()
+})
+
+// Update a new User
+app.put('/api/v1/users/:id', async (req, res) => {
+    const db = new sqlite3.Database(dbPath)
+    const id = req.params.id
+
+    db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+        const name = req.body.name ? req.body.name : row.name
+        const profile = req.body.profile ? req.body.profile : row.profile
+        const date_of_birth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+    })
+
+    await run(`UPDATE users SET name="${name}", profile="${profile}", date_of_birth=${date_of_birth}" WHERE id = ${id}`,
+    db,
+    res,
+     "ユーザー情報を更新しました。")
+    db.close()
+})
+
+app.delete('/api/v1/users/:id', async (req, res) => {
+    const db = new sqlite3.Database(dbPath)
+    const id = req.params.id
+
+    await run(`DELETE FROM users WHERE id = ${id}`,
+    db,
+    res,
+     "ユーザー情報を削除しました。")
+    db.close()
+})
+
+const run = async (sql, db, res, message) =>{
+    return new Promise((resolve, reject) =>{
+        db.run(sql, (err) =>{
+            if(err){
+                res.status(500).send(err)
+                return reject()
+            }else{
+                res.json({message:message})
+                return resolve()
+            }
+        })
+    })
+}
+
 
 const port = process.env.PORT || 3000;
 app.listen(port)
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,10 +1,30 @@
 const indexModule = (() => {
-    //検索ボタンをクリックしたときのイベントリスナー
-    document.getElementById('search-btn').addEventListener('click', () => {
-        return searchModule.searchUsers()
-    })
+    const path = window.location.pathname
 
-    // UserのfetchUsersメソッドを呼び出す
-    return usersModule.fetchAllUsers()
+    switch (path) {
+        case '/':
+            //検索ボタンをクリックしたときのイベントリスナー
+            document.getElementById('search-btn').addEventListener('click', () => {
+                return searchModule.searchUsers()
+            })
+
+            // UserのfetchUsersメソッドを呼び出す
+            return usersModule.fetchAllUsers()
+            
+        
+        case '/create_user.html':
+            document.getElementById('create-btn').addEventListener('click', () => {
+                return usersModule.createUser()
+            })
+
+            document.getElementById('cancel-btn').addEventListener('click', () => {
+                return window.location.href = '/'
+            })
+            break;
+        default:
+            break;
+
+
+    }
 })()
 
