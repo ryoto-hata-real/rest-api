7ee79e42770be94fb5b338551a4cc170ee355ef3
ryoto-hata-real <ryotoht1203@gmail.com>
2022-02-09 23:26:50 +0900

    error

--- app/app.js
+++ app/app.js
@@ -32,7 +32,11 @@ app.get('/api/v1/users/:id',(req, res) => {
     const id = req.params.id
 
     db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
-        res.json(row)
+        if(!row){
+            res.status(404).json({status:404,error:"Not Found"})
+        }else{
+            res.status(200).json(row)
+        }
     })
 
     db.close()
@@ -53,57 +57,75 @@ app.get('/api/v1/search', (req, res) => {
 
 // Create a new User
 app.post('/api/v1/users', async (req, res) => {
-    const db = new sqlite3.Database(dbPath)
+    if(!req.body.name || req.body.name === ""){
+        res.status(400).json({status:400,error:"UserName is not set"})
+    }else{
 
-    const name = req.body.name
-    const profile = req.body.profile ? req.body.profile : ""
-    const date_of_birth = req.body.date_of_birth ? req.body.date_of_birth : ""
+        const db = new sqlite3.Database(dbPath)
 
-    
+        const name = req.body.name
+        const profile = req.body.profile ? req.body.profile : ""
+        const date_of_birth = req.body.date_of_birth ? req.body.date_of_birth : ""
 
-    await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${date_of_birth}")`, db, res, "新規ユーザを作成しました。")
-    .then((code) => { console.log("normal:" + code); })
-    .catch((code) => { console.error("error:" + code); })
-    db.close()
+        
+        try {
+            await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${date_of_birth}")`, db)
+            res.status(201).json({status:201, message:"ユーザーを新規登録しました。"})
+        } catch (e){
+            res.status(500).json({status:500, error:e})
+        }
+        db.close()
+    }
 })
 
 // Update a new User
 app.put('/api/v1/users/:id', async (req, res) => {
-    const db = new sqlite3.Database(dbPath)
-    const id = req.params.id
+    if(!req.body.name || req.body.name === ""){
+        res.status(400).json({status:400,error:"UserName is not set"})
+    }else{
+        const db = new sqlite3.Database(dbPath)
+        const id = req.params.id
 
-    db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
-        const name = req.body.name ? req.body.name : row.name
-        const profile = req.body.profile ? req.body.profile : row.profile
-        const date_of_birth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
-    })
-
-    await run(`UPDATE users SET name="${name}", profile="${profile}", date_of_birth=${date_of_birth}" WHERE id = ${id}`,
-    db,
-    res,
-     "ユーザー情報を更新しました。")
-    db.close()
+        db.get(`SELECT * FROM users WHERE id = ${id}`, async (err, row) => {
+            if(!row){
+                res.status(404).json({status:404,error:"Not Found"})
+            }else{
+                const name = req.body.name ? req.body.name : row.name
+                const profile = req.body.profile ? req.body.profile : row.profile
+                const date_of_birth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+                try {
+                    await run(`UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${date_of_birth}" WHERE id = ${id}`,db)
+                    res.status(200).json({status:200, message:"ユーザー情報を変更しました。"})
+                } catch (e){
+                    res.status(500).json({status:500, error:e})
+                }
+            
+            }
+        })
+        db.close()
+    }
 })
 
 app.delete('/api/v1/users/:id', async (req, res) => {
     const db = new sqlite3.Database(dbPath)
     const id = req.params.id
 
-    await run(`DELETE FROM users WHERE id = ${id}`,
-    db,
-    res,
-     "ユーザー情報を削除しました。")
+    try {
+        await run(`DELETE FROM users WHERE id = ${id}`,db)
+        res.status(200).json({status:200, message:"ユーザー情報を削除しました。"})
+    } catch (e){
+        res.status(500).json({status:500, error:e})
+    }
+    
     db.close()
 })
 
-const run = async (sql, db, res, message) =>{
+const run = async (sql, db) =>{
     return new Promise((resolve, reject) =>{
         db.run(sql, (err) =>{
             if(err){
-                res.status(500).send(err)
-                return reject()
+                return reject(err)
             }else{
-                res.json({message:message})
                 return resolve()
             }
         })
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -15,7 +15,7 @@ const usersModule = (() =>{
             for (let i=0; i < users.length; i++){
                 const user = users[i]
                 const body = `<tr>
-                                <td>${user.id}</td>
+                                <td><a href="edit.html?uid=${user.id}">${user.id}</a></td>
                                 <td>${user.name}</td>
                                 <td>${user.profile}</td>
                                 <td>${user.date_of_birth}</td>
@@ -25,12 +25,23 @@ const usersModule = (() =>{
                 document.getElementById('users-list').insertAdjacentHTML('beforeend', body)
             }
         },
+        fetchOneUser: async (uid) => {
+
+            const res = await fetch(BASE_URL + "/" + uid)
+
+            // JSONをJSで使えるようにパース
+            const user = await res.json()
+            console.log(user)
+
+            document.getElementById('uid').value = user.id
+            document.getElementById('name').value = user.name
+            document.getElementById('profile').value = user.profile
+            document.getElementById('date-of-birth').value = user.date_of_birth  
+        },
         createUser: async () => {
             const name = document.getElementById("name").value
             const profile = document.getElementById("profile").value
             const dateOfBirth = document.getElementById("date-of-birth").value
-
-            console.log(name + ": " + profile + ": " + dateOfBirth)
 
             const body = {
                 name: name,
@@ -46,7 +57,48 @@ const usersModule = (() =>{
 
             const resJson = await res.json()
 
-            alert(resJson.message)
+            alert(resJson.status + ": " + resJson.message)
+            window.location.href = "/"
+        },
+
+        editUser: async (uid) => {
+            const name = document.getElementById("name").value
+            const profile = document.getElementById("profile").value
+            const dateOfBirth = document.getElementById("date-of-birth").value
+            const body = {
+                name: name,
+                profile: profile,
+                date_of_birth: dateOfBirth
+            }
+
+            const res = await fetch(BASE_URL + "/" + uid, {
+                method: "PUT",
+                headers: headers,
+                body: JSON.stringify(body)
+            })
+
+            const resJson = await res.json()
+
+            alert(resJson.status + ": " + resJson.message)
+            window.location.href = "/"
+        },
+
+        deleteUser: async (uid) => {
+            const confirmation = window.confirm('このユーザを削除しますか？')
+
+            if (!confirmation){
+                return false
+            }else{
+                const res = await fetch(BASE_URL+"/"+uid,  {
+                    method: "DELETE",
+                })
+                const resJson = await res.json()
+
+                alert(resJson.status + ": " + resJson.message)
+                window.location.href = "/"
+            }
+
+
         }
     }
 })()
